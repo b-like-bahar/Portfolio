86157ab180ea5be04002a131fc47e58468d60c4e
Bahar Hamzeh <blikebahar76@gmail.com>
2024-10-16 04:59:24 -0600

    add Skills component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header/Header.jsx";
 import Home from "./components/Home/Home.jsx";
+import Skills from "./components/Skills/Skills.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 import './App.scss'
 
@@ -9,6 +10,7 @@ function App() {
     <>
     <Header />
     <Home />
+    <Skills />
     <Footer />
     </>
   )
