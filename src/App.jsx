08e03db2846dd93ac125f2cfaf7f777918ad9077
Bahar Hamzeh <blikebahar76@gmail.com>
2024-10-17 03:30:41 -0600

    add about component to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header/Header.jsx";
 import Home from "./components/Home/Home.jsx";
+import About from "./components/About/About.jsx";
 import Skills from "./components/Skills/Skills.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 import './App.scss'
@@ -10,6 +11,7 @@ function App() {
     <>
     <Header />
     <Home />
+    <About />
     <Skills />
     <Footer />
     </>
