c109ea30b18d60b384ec2debb93aad201a24d041
Bahar Hamzeh <blikebahar76@gmail.com>
2024-10-18 22:54:04 -0600

    add contact component to app.js and wrap contact and footer components to a div tag

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./components/Home/Home.jsx";
 import About from "./components/About/About.jsx";
 import Projects from "./components/Projects/Projects.jsx";
 import Skills from "./components/Skills/Skills.jsx";
+import Contact from "./components/Contact/Contact.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 import './App.scss'
 
@@ -15,7 +16,10 @@ function App() {
     <About />
     <Projects />
     <Skills />
+    <div className="contact-footer-wrapper">
+    <Contact />
     <Footer />
+    </div>
     </>
   )
 }
