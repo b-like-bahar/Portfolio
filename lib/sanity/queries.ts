5decd7c229a392b6215f771a32105ec304c23c2e
Bahar Hamzeh <blikebahar76@gmail.com>
2026-01-19 04:02:59 -0600

    Add queries for blog posts and categories in Sanity CMS, including definitions for fetching blog lists, individual posts, and category lists.

---
+++ lib/sanity/queries.ts
@@ -0,0 +1,43 @@
+import { defineQuery, groq } from "next-sanity";
+
+export const BLOG_LIST_QUERY = defineQuery(
+  groq`*[
+  _type == "post" 
+  && defined(slug.current)
+  ] | order(publishedAt desc) {
+    _id,
+    title,
+    publishedAt,
+    slug,
+    category -> {name},
+    image,
+  }`
+);
+
+export const BLOG_POST_QUERY = defineQuery(
+  groq`*[
+  _type == "post" 
+  && defined(slug.current) 
+  && slug.current == $slug
+  ] | order(publishedAt desc) [0] {
+    _id,
+    title,
+    slug,
+    publishedAt,
+    category -> {name, slug},
+    description,
+    image,
+    body
+  }`
+);
+
+export const CATEGORY_LIST_QUERY = defineQuery(
+  groq`*[
+  _type == "category" 
+  && defined(slug.current)
+  ] | order(name asc) {
+    _id,
+    name,
+    slug
+  }`
+);
