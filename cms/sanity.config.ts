6aafdddaebcf58457edbb1593f8eda49006084d6
Bahar Hamzeh <blikebahar76@gmail.com>
2026-01-19 23:17:03 -0600

    Add structure tool plugin to Sanity configuration for enhanced content organization.

--- cms/sanity.config.ts
+++ cms/sanity.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
 import { schemaTypes } from "./schemaTypes";
 
 export default defineConfig({
   projectId: "be2t44ka",
   dataset: "production",
+  plugins: [structureTool()],
   schema: {
     types: schemaTypes,
   },
